6054e6194155c0eb6292d2010e9d2c90cab63937
Niloy-Dey <niloydeyce@gmail.com>
2023-09-21 10:56:27 +0600

    trip dropdown searchbar done

--- src/app/(Dashboard)/dashboard/invoice/addInvoice/page.jsx
+++ src/app/(Dashboard)/dashboard/invoice/addInvoice/page.jsx
@@ -1,11 +1,52 @@
+'use client'
+
+
 import Image from 'next/image';
 import { FaAngleDown, FaCalendar } from 'react-icons/fa';
 import upload from '/public/images/upload.png'
 import increase from '/public/images/increase.png'
 import Ddelete from '/public/images/delete.png'
-
+import search from '/public/images/search.png'
 import AddProduct from '../addProduct/page'
+import { useState } from 'react';
 const addInvoice = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    // const [isOpenD, setIsOpenD] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+    // const [selectedOptionD, setSelectedOptionD] = useState('');
+    const [searchText, setSearchText] = useState('');
+    // const [searchTextD, setSearchTextD] = useState('');
+    const options = ['T2390', 'TR7652', 'TR7652']; // Replace with your actual options
+
+
+
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleSelectChange = (e) => {
+        setSelectedOption(e.target.value);
+        setIsOpen(false);
+    };
+
+    const handleInputChange = (e) => {
+        setSearchText(e.target.value);
+    };
+
+
+
+    const toggleTypeDropdown = () => {
+        setIsOpenD(!isOpenD);
+    };
+    const handleTypeSelectChange = (e) => {
+        setSelectedOptionD(e.target.value);
+        setIsOpenD(false);
+    };
+    const handleTypeInputChange = (e) => {
+        setSearchTextD(e.target.value);
+    };
 
 
 
@@ -55,19 +96,51 @@ const addInvoice = () => {
 
 
                     </div>
+
                     <div className='w-[254px] mt-16 h-[60px] '>
-                        <label for="dropdown" class="font-poppins pl-1 font-bold text-lg text-black ">Trip</label>
-                        <div class="relative  border border-gray-300 rounded-full mt-4 p-3">
-                            <select id="dropdown" class="appearance-none bg-transparent border-none outline-none w-full pr-8 pt-1 font-poppins text-lg pl-2">
-                                <option>T2390</option>
-                                <option>TR7652</option>
-                                <option>TR7652</option>
-                            </select>
-                            <div class="absolute inset-y-0 right-0 flex items-center  pointer-events-none">
-                                <FaAngleDown className="h-[20px] w-[20px] mr-4 text-gray-400" />
+                        <label htmlFor="dropdown" className="font-poppins pl-1 font-bold text-lg text-black ">
+                            Trip
+                        </label>
+                        <div className="relative border border-gray-300 rounded-full mt-4 p-3">
+                            <div className="relative">
+                                <input
+                                    type="text"
+                                    placeholder="Trip"
+                                    className="w-full outline-none bg-transparent pr-8 pt-1 font-poppins text-lg pl-2"
+                                    onClick={toggleDropdown}
+                                    readOnly
+                                />
+
                             </div>
+                            {isOpen && (
+                                <div className="absolute z-10 top-14 left-0 right-0 border border-gray-300 bg-white rounded-b-lg">
+                                    <div>
+                                        <div className='relative flex justify-between items-center'>
+                                            <div className="absolute inset-y-0 left-5 flex items-center pointer-events-none">
+                                                <Image src={search} alt='' className="h-5 w-5 mr-4 text-gray-400" />
+                                            </div>
+
+                                            <input
+                                                type="text"
+                                                placeholder="   Search options"
+                                                className="pl-4 py-4 m-2 outline-none rounded-full w-full border-2 border-gray-300 p-2"
+                                                onChange={handleInputChange}
+                                            />
+
+                                        </div>
+
+                                        <p className='px-2 py-2 font-poppins text-lg '>T2390</p>
+                                        <p className='px-2 py-2 font-poppins text-lg '>T2390</p>
+                                        <p className='px-2 py-2 font-poppins text-lg '>T2390</p>
+                                        <p className='px-2 py-2 font-poppins text-lg '>T2390</p>
+                                        <p className='px-2 py-2 font-poppins text-lg '>T2390</p>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
+
+
                 </div>
 
 
@@ -98,7 +171,7 @@ const addInvoice = () => {
                 <table className="table-auto mb-10 w-full text-left whitespace-no-wrap">
                     <thead>
                         <tr className=" flex border-t-2 border-b-2 border-gray-100">
-                            <th className="font-poppins   w-[170px] flex items-center py-5 font-bold text-center font-poppins text-gray-900 text-sm ">Products Type<Image className=" ml-1" alt="" src={increase} /></th>
+                            <th htmlFor="dropdown" className="font-poppins   w-[170px] flex items-center py-5 font-bold text-center font-poppins text-gray-900 text-sm ">Products Type<Image className=" ml-1" alt="" src={increase} /></th>
                             <th className="font-poppins  w-[260px] flex items-center  py-5 font-bold text-center font-poppins text-gray-900 text-sm">Products<Image className=" ml-1" alt="" src={increase} /> </th>
                             <th className="font-poppins  w-[390px]   py-5 font-bold text-center font-poppins text-gray-900 text-sm"> Description </th>
                             <th className="font-poppins ml-52 py-5 font-bold text-center font-poppins text-gray-900 text-sm"> Total</th>
